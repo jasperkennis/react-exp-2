8f8fab349feb04b1e5567fa69426f2394d2ebdaf
Jasper Kennis <contact@jasperkennis.nl>
2018-07-11 15:16:18 +0200

    pass the get products function and call it

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import https from 'https';
+import jQuery from 'jquery';
 import {decrement, decrementAsync, increment, incrementAsync} from '../../modules/counter';
 import CategoriesList from './categories-list';
 import ProductGrid from './product-grid';
@@ -7,34 +10,79 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {push} from 'react-router-redux';
 
-const Home = (props) => (
-    <div>
+/**
+ * [getProducts description]
+ * @param  {[type]} props [description]
+ * @return {[type]}       [description]
+ */
+function getProducts (props) {
+  if (props.products.length > 0 || props.funnel.complete) {
+    return null;
+  }
+
+  const agent = axios.create({
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false
+    })
+  });
+
+  agent.get('https://www.drukzo.nl.local/uitgelicht').then((res) => {
+    // const html = res.data.replace(/<img[^>]*>/g, '');
+    const html = res.data;
+    const _$obj = jQuery(html);
+    const _$items = _$obj.find('.normal-categories a.standard');
+    const products = [];
+    const link_parser = document.createElement('a');
+    let _$;
+
+    _$items.each(function (i, e) {
+      _$ = jQuery(e);
+
+      if (_$.text() && _$.attr('href')) {
+        link_parser.href = _$.attr('href');
+        products.push({
+          url: link_parser.pathname,
+          name: _$.find('.title').text(),
+          image_src: _$.find('picture img').attr('src'),
+          image_srcset: _$.find('picture source').attr('srcset')
+        });
+      }
+    });
+
+    props.productsSet(products);
+  });
+}
+const Home = (props) => {
+    getProducts(props);
+    return (
       <div>
-        <div className="cell">
-          <article className="article">
-            <CategoriesList />
-            <ProductGrid {...props} />
-          </article>
+        <div>
+          <div className="cell">
+            <article className="article">
+              <CategoriesList />
+              <ProductGrid {...props} />
+            </article>
+          </div>
         </div>
+        <h1>Home</h1>
+        <p>Welcome home!</p>
+        <p>Count: {props.count}</p>
+
+        <p>
+          <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+          <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
+        </p>
+
+        <p>
+          <button onClick={props.decrement} disabled={props.isDecrementing}>Decrementing</button>
+          <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
+        </p>
+
+
+        <button onClick={() => props.changePage()}>Go to about page via redux</button>
       </div>
-      <h1>Home</h1>
-      <p>Welcome home!</p>
-      <p>Count: {props.count}</p>
-
-      <p>
-        <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
-        <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
-      </p>
-
-      <p>
-        <button onClick={props.decrement} disabled={props.isDecrementing}>Decrementing</button>
-        <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
-      </p>
-
-
-      <button onClick={() => props.changePage()}>Go to about page via redux</button>
-    </div>
-  ),
+    );
+  },
   mapStateToProps = (state) => ({
     count: state.counter.count,
     isIncrementing: state.counter.isIncrementing,
